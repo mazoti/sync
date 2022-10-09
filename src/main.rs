411a5d21396bfeef0c03f7310e2f4f76aa6e5cd6
Marcos <mazoti@mail.com>
2022-10-08 22:45:30 -0300

    main refactored

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 mod processor;
 
+const FN_ARGS: [fn(); 4] = [one_argument, two_arguments, three_arguments, four_arguments];
+
 /// Display the error message (optional) and send the error code to operating system
 macro_rules! error {
     ($err:expr) => {
@@ -29,122 +31,123 @@ macro_rules! no_error {
     };
 }
 
-/// Process user input from command line
-fn main() {
+fn four_arguments() {
     #[cfg(feature = "cli")]
     let _start = std::time::Instant::now();
 
-    if std::env::args().len() == 1 {
+    let command = std::env::args().nth(1).unwrap();
+    let source_folder = std::env::args().nth(2).unwrap();
+    let dest_folder = std::env::args().nth(3).unwrap();
+
+    if command == "--check" || command == "-check" || command == "check" || command == "-c" {
         #[cfg(feature = "cli")]
-        processor::cli::show_header(true);
-        let current_path = std::env::current_dir().unwrap().display().to_string();
-        if let Err(err) = processor::sync::folder(&current_path) {
-            #[cfg(feature = "cli")]
-            if err.code == processor::consts::HELP {
-                std::process::exit(processor::cli::help());
-            }
+        processor::cli::show_header(false);
+
+        if let Err(err) = processor::check::check(&source_folder, &dest_folder) {
             error!(err);
         }
         no_error!(_start);
     }
 
-    if std::env::args().len() == 2 {
-        let config = std::env::args().nth(1).unwrap();
-
+    if command == "--force" || command == "-force" || command == "force" || command == "-f" {
         #[cfg(feature = "cli")]
-        {
-            if config == "--help"
-                || config == "-h"
-                || config == "/?"
-                || config == "/help"
-                || config == "-help"
-                || config == "help"
-            {
-                std::process::exit(processor::cli::help());
-            }
+        processor::cli::show_header(false);
 
-            if config == "--version"
-                || config == "-version"
-                || config == "version"
-                || config == "-v"
-            {
-                println!("{}", option_env!("CARGO_PKG_VERSION").unwrap_or("unknown"));
-                std::process::exit(processor::consts::NO_ERROR);
-            }
+        processor::force(&source_folder, &dest_folder);
+        no_error!(_start);
+    }
 
-            processor::cli::show_header(true);
-        }
+    #[cfg(feature = "cli")]
+    if command == "--simulate" || command == "-simulate" || command == "simulate" || command == "-s"
+    {
+        processor::cli::show_header(false);
 
-        if let Err(err) = processor::sync::file(&config) {
+        if let Err(err) = processor::sync::simulate(&source_folder, &dest_folder) {
             error!(err);
         }
         no_error!(_start);
     }
 
-    if std::env::args().len() == 3 {
-        let source = std::env::args().nth(1).unwrap();
-        let destination = std::env::args().nth(2).unwrap();
+    #[cfg(feature = "cli")]
+    processor::cli::show_header(true);
 
-        #[cfg(feature = "cli")]
-        processor::cli::show_header(true);
-        if let Err(err) = processor::sync::sync(&source, &destination) {
-            error!(err);
+    if let Err(err) = processor::create(&command, &source_folder, &dest_folder) {
+        error!(err);
+    }
+    no_error!(_start);
+}
+
+fn three_arguments() {
+    #[cfg(feature = "cli")]
+    let _start = std::time::Instant::now();
+
+    let source = std::env::args().nth(1).unwrap();
+    let destination = std::env::args().nth(2).unwrap();
+
+    #[cfg(feature = "cli")]
+    processor::cli::show_header(true);
+    if let Err(err) = processor::sync::sync(&source, &destination) {
+        error!(err);
+    }
+    no_error!(_start);
+}
+
+fn two_arguments() {
+    #[cfg(feature = "cli")]
+    let _start = std::time::Instant::now();
+
+    let config = std::env::args().nth(1).unwrap();
+
+    #[cfg(feature = "cli")]
+    {
+        if config == "--help"
+            || config == "-h"
+            || config == "/?"
+            || config == "/help"
+            || config == "-help"
+            || config == "help"
+        {
+            std::process::exit(processor::cli::help());
         }
-        no_error!(_start);
+
+        if config == "--version" || config == "-version" || config == "version" || config == "-v" {
+            println!("{}", option_env!("CARGO_PKG_VERSION").unwrap_or("unknown"));
+            std::process::exit(processor::consts::NO_ERROR);
+        }
+
+        processor::cli::show_header(true);
     }
 
-    if std::env::args().len() == 4 {
-        let source_folder = std::env::args().nth(1).unwrap();
-        let dest_folder = std::env::args().nth(2).unwrap();
-        let config = std::env::args().nth(3).unwrap();
+    if let Err(err) = processor::sync::file(&config) {
+        error!(err);
+    }
+    no_error!(_start);
+}
 
-        if source_folder == "--check"
-            || source_folder == "-check"
-            || source_folder == "check"
-            || source_folder == "-c"
-        {
-            #[cfg(feature = "cli")]
-            processor::cli::show_header(false);
+fn one_argument() {
+    #[cfg(feature = "cli")]
+    let _start = std::time::Instant::now();
 
-            if let Err(err) = processor::check::check(&dest_folder, &config) {
-                error!(err);
-            }
-            no_error!(_start);
-        }
+    #[cfg(feature = "cli")]
+    processor::cli::show_header(true);
 
-        if source_folder == "--force"
-            || source_folder == "-force"
-            || source_folder == "force"
-            || source_folder == "-f"
-        {
-            #[cfg(feature = "cli")]
-            processor::cli::show_header(false);
-
-            processor::force(&dest_folder, &config);
-            no_error!(_start);
-        }
-
+    let current_path = std::env::current_dir().unwrap().display().to_string();
+    if let Err(err) = processor::sync::folder(&current_path) {
         #[cfg(feature = "cli")]
-        if source_folder == "--simulate"
-            || source_folder == "-simulate"
-            || source_folder == "simulate"
-            || source_folder == "-s"
-        {
-            processor::cli::show_header(false);
-
-            if let Err(err) = processor::sync::simulate(&dest_folder, &config) {
-                error!(err);
-            }
-            no_error!(_start);
+        if err.code == processor::consts::HELP {
+            std::process::exit(processor::cli::help());
         }
+        error!(err);
+    }
+    no_error!(_start);
+}
 
-        #[cfg(feature = "cli")]
-        processor::cli::show_header(true);
+/// Process user input from command line
+fn main() {
+    let args = std::env::args().len();
 
-        if let Err(err) = processor::create(&source_folder, &dest_folder, &config) {
-            error!(err);
-        }
-        no_error!(_start);
+    if args < 6 {
+        return FN_ARGS[args - 1]();
     }
 
     #[cfg(feature = "cli")]

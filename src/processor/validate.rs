3da5d4801ebad5ec1006fcf0990c8c27ff3297df
Marcos <mazoti@gmail.com>
2022-12-14 15:51:41 -0300

    empty merged
    Signed-off-by: Marcos <mazoti@gmail.com>

--- src/processor/validate.rs
+++ src/processor/validate.rs
@@ -2,7 +2,6 @@ pub fn empty(folder: &str) -> Result<(), crate::processor::SyncError> {
     let mut count: usize;
     let mut folder_metadata: std::fs::Metadata;
 
-    let mut fullpath: String = "".to_string();
     let folder_path = std::path::Path::new(&folder);
 
     if !folder_path.exists() {
@@ -30,7 +29,7 @@ pub fn empty(folder: &str) -> Result<(), crate::processor::SyncError> {
 
     count = 0;
     for path in std::fs::read_dir(folder)? {
-        fullpath = path?.path().display().to_string();
+        let fullpath = path?.path().display().to_string();
         folder_metadata = std::fs::metadata(&fullpath)?;
 
         if folder_metadata.is_dir() {
@@ -47,12 +46,12 @@ pub fn empty(folder: &str) -> Result<(), crate::processor::SyncError> {
     }
 
     if count == 0 {
-        crate::processor::empty_msg(&fullpath);
+        crate::processor::empty_msg(folder);
         return Ok(());
     }
 
     if count == 1 {
-        crate::processor::one_item(&fullpath);
+        crate::processor::one_item(folder);
     }
 
     Ok(())

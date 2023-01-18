d22bce7a826d19a5ac4cfd3d558c289f9ddfaa67
Marcos Mazoti <mazoti@gmail.com>
2023-01-18 17:16:03 -0300

    Check mt (#40)
    * Error refactored
    
    Signed-off-by: Marcos <mazoti@gmail.com>
    
    * split and join patched
    
    Signed-off-by: Marcos <mazoti@gmail.com>
    
    * Error messages patched
    
    Signed-off-by: Marcos <mazoti@gmail.com>
    
    * I18n refactored
    
    Signed-off-by: Marcos <mazoti@gmail.com>
    
    * Copy feature added
    
    Signed-off-by: Marcos <mazoti@gmail.com>
    
    * Downloads updated
    
    Signed-off-by: Marcos <mazoti@gmail.com>
    
    * Hashs and duplicated features added
    
    Signed-off-by: Marcos <mazoti@gmail.com>
    
    * Buffer refactor
    
    Signed-off-by: Marcos <mazoti@gmail.com>
    
    * Copy refactored
    
    Signed-off-by: Marcos <mazoti@gmail.com>
    
    * Check multithread feature added
    
    Signed-off-by: Marcos <mazoti@gmail.com>
    
    Signed-off-by: Marcos <mazoti@gmail.com>

--- src/processor/check.rs
+++ src/processor/check.rs
@@ -5,133 +5,12 @@
 use std::{io::Read, path::Path};
 
 /// Compares every folder, file and byte
+#[cfg(not(feature = "check-mt"))]
 pub fn check(
     source: &str,
     destination: &str,
     buffer_size: u64,
 ) -> Result<(), crate::processor::SyncError> {
-    /// Look for removed files or folders in source
-    fn check_file_folder_add_removed(
-        source: &str,
-        destination: &str,
-    ) -> Result<(), crate::processor::SyncError> {
-        // Check for empty folder
-        if std::fs::read_dir(source)?.next().is_none() {
-            if std::fs::read_dir(destination)?.next().is_none() {
-                return Ok(());
-            }
-            return Err(crate::processor::SyncError {
-                code: crate::processor::error_diff_file_folder(),
-                file: file!(),
-                line: line!(),
-                source: Some(source.to_string()),
-                destination: Some(destination.to_string()),
-            });
-        }
-
-        for path in std::fs::read_dir(source)? {
-            let fullpath = path?.path().display().to_string();
-            let fullpath_destination = fullpath.replace(source, destination);
-
-            // Check file or symlink
-            if !Path::new(&fullpath).is_dir() {
-                if !Path::new(&fullpath_destination).exists() {
-                    return Err(crate::processor::SyncError {
-                        code: crate::processor::error_diff_file_folder(),
-                        file: file!(),
-                        line: line!(),
-                        source: Some(source.to_string()),
-                        destination: Some(destination.to_string()),
-                    });
-                }
-                continue;
-            }
-            check_file_folder_add_removed(&fullpath, &fullpath_destination)?;
-        }
-        Ok(())
-    }
-
-    /// Checks if folder and file contents are the same
-    fn check_file_folder(
-        source: &str,
-        destination: &str,
-        buffer_size: u64,
-    ) -> Result<(), crate::processor::SyncError> {
-        // Check for empty folder
-        if std::fs::read_dir(source)?.next().is_none() {
-            if std::fs::read_dir(destination)?.next().is_none() {
-                return Ok(());
-            }
-            return Err(crate::processor::SyncError {
-                code: crate::processor::error_diff_file_folder(),
-                file: file!(),
-                line: line!(),
-                source: Some(source.to_string()),
-                destination: Some(destination.to_string()),
-            });
-        }
-
-        for path in std::fs::read_dir(source)? {
-            let fullpath = path?.path().display().to_string();
-            let fullpath_destination = fullpath.replace(source, destination);
-
-            // Check file or symlink
-            if !Path::new(&fullpath).is_dir() {
-                check_file(&fullpath, &fullpath_destination, buffer_size)?;
-                continue;
-            }
-            check_file_folder(&fullpath, &fullpath_destination, buffer_size)?;
-        }
-        Ok(())
-    }
-
-    /// Checks if file contents are the same
-    fn check_file(
-        source: &str,
-        destination: &str,
-        buffer_size: u64,
-    ) -> Result<(), crate::processor::SyncError> {
-        let mut src_file = std::fs::File::open(source)?;
-        let mut dest_file = std::fs::File::open(destination)?;
-
-        let buffer_usize = buffer_size.try_into()?;
-
-        let mut src_buffer = vec![0; buffer_usize];
-        let mut dest_buffer = vec![0; buffer_usize];
-
-        loop {
-            let src_bytes = src_file.read(&mut src_buffer)?;
-            let dest_bytes = dest_file.read(&mut dest_buffer)?;
-
-            if src_bytes != dest_bytes {
-                return Err(crate::processor::SyncError {
-                    code: crate::processor::error_diff_file_folder(),
-                    file: file!(),
-                    line: line!(),
-                    source: Some(source.to_string()),
-                    destination: Some(destination.to_string()),
-                });
-            }
-
-            for i in 0..src_bytes {
-                if src_buffer[i] != dest_buffer[i] {
-                    return Err(crate::processor::SyncError {
-                        code: crate::processor::error_diff_file_folder(),
-                        file: file!(),
-                        line: line!(),
-                        source: Some(source.to_string()),
-                        destination: Some(destination.to_string()),
-                    });
-                }
-            }
-
-            if src_bytes < buffer_usize {
-                break;
-            }
-        }
-        Ok(())
-    }
-
     if !(Path::new(&source).exists() && Path::new(&destination).exists()) {
         return Err(crate::processor::SyncError {
             code: crate::processor::error_source_folder(),
@@ -182,6 +61,261 @@ pub fn check(
     }
 
     check_file(source, destination, buffer_size)
+}
+
+/// Compares every folder, file and byte
+#[cfg(feature = "check-mt")]
+pub fn check(
+    source: &str,
+    destination: &str,
+    buffer_size: u64,
+) -> Result<(), crate::processor::SyncError> {
+    let destination1: String;
+    let source1: String;
+    let destination2: String;
+    let source2: String;
+
+    let handle1: std::thread::JoinHandle<Result<(), crate::processor::SyncError>>;
+    let handle2: std::thread::JoinHandle<Result<(), crate::processor::SyncError>>;
+
+    if source == destination {
+        return Err(crate::processor::SyncError {
+            code: crate::processor::error_same_file_folder(),
+            file: file!(),
+            line: line!(),
+            source: Some(source.to_string()),
+            destination: Some(destination.to_string()),
+        });
+    }
+
+    if !(Path::new(&source).exists() && Path::new(&destination).exists()) {
+        return Err(crate::processor::SyncError {
+            code: crate::processor::error_source_folder(),
+            file: file!(),
+            line: line!(),
+            source: Some(source.to_string()),
+            destination: Some(destination.to_string()),
+        });
+    }
+
+    // source and destination are folders
+    if Path::new(&source).is_dir() {
+        if Path::new(&destination).is_dir() {
+            destination1 = String::from(destination);
+            source1 = String::from(source);
+            destination2 = String::from(destination);
+            source2 = String::from(source);
+
+            handle1 = std::thread::spawn(move || -> Result<(), crate::processor::SyncError> {
+                check_file_folder_add_removed(&destination1, &source1)?;
+                Ok(())
+            });
+
+            handle2 = std::thread::spawn(move || -> Result<(), crate::processor::SyncError> {
+                check_file_folder_add_removed(&source2, &destination2)?;
+                Ok(())
+            });
+
+            match handle1.join() {
+                Err(_) => {
+                    if let Err(_) = handle2.join() {
+                        return Err(crate::processor::SyncError {
+                            code: crate::processor::error_thread_join(),
+                            file: file!(),
+                            line: line!(),
+                            source: None,
+                            destination: None,
+                        });
+                    }
+                    return Err(crate::processor::SyncError {
+                        code: crate::processor::error_thread_join(),
+                        file: file!(),
+                        line: line!(),
+                        source: None,
+                        destination: None,
+                    });
+                }
+                Ok(value) => match handle2.join() {
+                    Err(_) => {
+                        return Err(crate::processor::SyncError {
+                            code: crate::processor::error_thread_join(),
+                            file: file!(),
+                            line: line!(),
+                            source: None,
+                            destination: None,
+                        });
+                    }
+                    Ok(value2) => {
+                        if let Ok(_) = value {
+                            if let Ok(_) = value2 {
+                                return check_file_folder(source, destination, buffer_size);
+                            }
+                        }
+                    }
+                },
+            }
+
+            return Err(crate::processor::SyncError {
+                code: crate::processor::error_diff_file_folder(),
+                file: file!(),
+                line: line!(),
+                source: Some(source.to_string()),
+                destination: Some(destination.to_string()),
+            });
+        }
+
+        // source is a directory but destination is not
+        return Err(crate::processor::SyncError {
+            code: crate::processor::error_dest_not_folder(),
+            file: file!(),
+            line: line!(),
+            source: Some(source.to_string()),
+            destination: Some(destination.to_string()),
+        });
+    }
+
+    // source is a file or symlink
+    if Path::new(&destination).is_dir() {
+        return Err(crate::processor::SyncError {
+            code: crate::processor::error_dest_not_file(),
+            file: file!(),
+            line: line!(),
+            source: Some(source.to_string()),
+            destination: Some(destination.to_string()),
+        });
+    }
+
+    check_file(source, destination, buffer_size)
+}
+
+/// Checks if file contents are the same
+fn check_file(
+    source: &str,
+    destination: &str,
+    buffer_size: u64,
+) -> Result<(), crate::processor::SyncError> {
+    let mut src_bytes: usize;
+    let mut dest_bytes: usize;
+
+    let mut src_file = std::fs::File::open(source)?;
+    let mut dest_file = std::fs::File::open(destination)?;
+
+    let buffer_usize = buffer_size.try_into()?;
+
+    let mut src_buffer = vec![0; buffer_usize];
+    let mut dest_buffer = vec![0; buffer_usize];
+
+    loop {
+        src_bytes = src_file.read(&mut src_buffer)?;
+        dest_bytes = dest_file.read(&mut dest_buffer)?;
+
+        if src_bytes != dest_bytes {
+            return Err(crate::processor::SyncError {
+                code: crate::processor::error_diff_file_folder(),
+                file: file!(),
+                line: line!(),
+                source: Some(source.to_string()),
+                destination: Some(destination.to_string()),
+            });
+        }
+
+        for i in 0..src_bytes {
+            if src_buffer[i] != dest_buffer[i] {
+                return Err(crate::processor::SyncError {
+                    code: crate::processor::error_diff_file_folder(),
+                    file: file!(),
+                    line: line!(),
+                    source: Some(source.to_string()),
+                    destination: Some(destination.to_string()),
+                });
+            }
+        }
+
+        if src_bytes < buffer_usize {
+            return Ok(());
+        }
+    }
+}
+
+/// Checks if folder and file contents are the same
+fn check_file_folder(
+    source: &str,
+    destination: &str,
+    buffer_size: u64,
+) -> Result<(), crate::processor::SyncError> {
+    let mut fullpath: String;
+    let mut fullpath_destination: String;
+
+    // Check for empty folder
+    if std::fs::read_dir(source)?.next().is_none() {
+        if std::fs::read_dir(destination)?.next().is_none() {
+            return Ok(());
+        }
+        return Err(crate::processor::SyncError {
+            code: crate::processor::error_diff_file_folder(),
+            file: file!(),
+            line: line!(),
+            source: Some(source.to_string()),
+            destination: Some(destination.to_string()),
+        });
+    }
+
+    for path in std::fs::read_dir(source)? {
+        fullpath = path?.path().display().to_string();
+        fullpath_destination = fullpath.replace(source, destination);
+
+        // Check file or symlink
+        if !Path::new(&fullpath).is_dir() {
+            check_file(&fullpath, &fullpath_destination, buffer_size)?;
+            continue;
+        }
+        check_file_folder(&fullpath, &fullpath_destination, buffer_size)?;
+    }
+    Ok(())
+}
+
+/// Look for removed files or folders in source
+fn check_file_folder_add_removed(
+    source: &str,
+    destination: &str,
+) -> Result<(), crate::processor::SyncError> {
+    let mut fullpath: String;
+    let mut fullpath_destination: String;
+
+    // Check for empty folder
+    if std::fs::read_dir(source)?.next().is_none() {
+        if std::fs::read_dir(destination)?.next().is_none() {
+            return Ok(());
+        }
+        return Err(crate::processor::SyncError {
+            code: crate::processor::error_diff_file_folder(),
+            file: file!(),
+            line: line!(),
+            source: Some(source.to_string()),
+            destination: Some(destination.to_string()),
+        });
+    }
+
+    for path in std::fs::read_dir(source)? {
+        fullpath = path?.path().display().to_string();
+        fullpath_destination = fullpath.replace(source, destination);
+
+        // Check file or symlink
+        if !Path::new(&fullpath).is_dir() {
+            if !Path::new(&fullpath_destination).exists() {
+                return Err(crate::processor::SyncError {
+                    code: crate::processor::error_diff_file_folder(),
+                    file: file!(),
+                    line: line!(),
+                    source: Some(source.to_string()),
+                    destination: Some(destination.to_string()),
+                });
+            }
+            continue;
+        }
+        check_file_folder_add_removed(&fullpath, &fullpath_destination)?;
+    }
+    Ok(())
 }
 
 //====================================== Unit Tests ======================================
